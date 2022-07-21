f3296acc2adbdaa229a7b4f0edfa6bdbd9406042
Hambak-note <ghkd910205@gamil.com>
2022-07-21 16:21:14 +0900

    modify: 정수로 인덱스를 주던 부분을 iterator를 사용하도록 변경_결과:정답

--- beakjoon/src/n1406.java
+++ beakjoon/src/n1406.java
@@ -1,58 +1,70 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class n1406 {
     public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String inputStr = "";
         int commandCount = 0;
         inputStr = br.readLine();
         commandCount = Integer.parseInt(br.readLine());
 
         LinkedList<Character> strList = new LinkedList<>();
-        String answer = "";
+
 
         //LinkedList에 문자열 입력
         for(int i = 0; i < inputStr.length(); i++){
             strList.add(inputStr.charAt(i));
         }
 
-        //최초의 커서 위치는 마지막 문자열 뒤이기 때문에 strList의 사이즈 크기에 위치를 준다
-        int currentPosition = strList.size();
+        //iterator 생성
+        ListIterator<Character> currentPosition = strList.listIterator();
+        while(currentPosition.hasNext()){
+            currentPosition.next();
+        }
 
         for(int i = 0; i < commandCount; i++) {
             String command = br.readLine();
             char c = command.charAt(0);
 
-            if (c == 'L' && currentPosition != 0) {
-                currentPosition = currentPosition - 1;
-                continue;
-            }
-            if (c == 'D' && currentPosition != strList.size()) {
-                currentPosition = currentPosition + 1;
-                continue;
-            }
-            if (c == 'B' && currentPosition != 0) {
-                strList.remove(currentPosition - 1);
-                currentPosition = currentPosition - 1;
-                continue;
-            }
-            if (c == 'P') {
-                char newChar = command.charAt(2);
-                strList.add(currentPosition, newChar);
-                currentPosition = currentPosition + 1;
-                continue;
+            switch (c){
+                case 'L':
+                    if(currentPosition.hasPrevious())
+                        currentPosition.previous();
+
+                    break;
+                case 'D':
+                    if(currentPosition.hasNext())
+                        currentPosition.next();
+
+                    break;
+                case 'B':
+                    if(currentPosition.hasPrevious()) {
+                        currentPosition.previous();
+                        currentPosition.remove();
+                    }
+                    break;
+                case 'P':
+                    char t = command.charAt(2);
+                    currentPosition.add(t);
+
+                    break;
+                default:
+                    break;
             }
         }
 
-        for(int i=0; i < strList.size(); i++){
-            answer += strList.get(i);
+        for(Character tmp : strList) {
+            bw.write(tmp);
         }
 
-        System.out.println(answer);
+        bw.flush();
+        bw.close();
+
     }
 }
 
